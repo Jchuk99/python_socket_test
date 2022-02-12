f87990be76b1ba6cc71d789dacc411bd33a809ec
Jason Chukwu <jochuk95@gmail.com>
2022-02-12 14:39:23 -0500

    client/server can be closed with keyboard interrupt

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import socket
-import threading
+from threading import Thread, Event
+from time import sleep
 
 
 HEADER = 64
@@ -14,32 +15,34 @@ tel_client.connect(ADDR)
 map_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 map_client.connect(ADDR)
 
-def send(msg):
-    message = msg.encode('utf-8')
-    msg_length = len(message)
-    send_length = str(msg_length).encode('utf-8')
-    padded_send_length = send_length + b' ' * (HEADER - len(send_length))
-    client.send(padded_send_length)
-    client.send(message)
-    print(client.recv(2048).decode('utf-8'))
+event = Event()
 
-def request_tele(client):
+def request_data(client, msg):
     while True:
-        request_msg = f"{str(0)}|TEL".encode('utf-8')
-        padded_request_msg = request_msg + b' ' * (HEADER - len(request_msg))
-        client.send(padded_request_msg)
-        print(client.recv(2048).decode('utf-8'))
+        try:
+            request_msg = msg.encode('utf-8')
+            padded_request_msg = request_msg + b' ' * (HEADER - len(request_msg))
+            client.send(padded_request_msg)
+            print(client.recv(2048).decode('utf-8'))
+        except (ConnectionAbortedError, OSError) as e:
+            print("user is breaking client connection.")
+            break
 
-def request_map(client):
-    while True:
-        request_msg = f"{str(0)}|MAP".encode('utf-8')
-        padded_request_msg = request_msg + b' ' * (HEADER - len(request_msg))
-        client.send(padded_request_msg)
-        print(client.recv(2048).decode('utf-8'))
+print('starting threads')
+telemetry_msg = f"{str(0)}|TEL"
+map_msg = f"{str(0)}|MAP"
 
-thread = threading.Thread(target = request_tele, args=(tel_client,))
+thread = Thread(target = request_data, args=(tel_client, telemetry_msg))
 thread.start()
-thread = threading.Thread(target = request_map, args=(map_client,))
+thread = Thread(target = request_data, args=(map_client, map_msg))
 thread.start()
+print('threads started')
 
-print('done')
+while True:
+    try:
+        sleep(1)
+    except KeyboardInterrupt:
+        print('keyboard interrupt')
+        tel_client.close()
+        map_client.close()
+        break
--- server.py
+++ server.py
@@ -1,12 +1,13 @@
 import socket
 import threading
+from threading import Event
+from time import sleep
 import time
 
 # 64 byte header is sent before every message, dictating how long the next message will be
 HEADER = 64
 PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
-print(SERVER)
 ADDR = (SERVER, PORT)
 DISCONNECT_MESSAGE = "!DISCONNECT"
 
@@ -14,44 +15,60 @@ DISCONNECT_MESSAGE = "!DISCONNECT"
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
+event = Event()
+
 def handle_client(conn, addr):
     print(f"[NEW CONNECTION] {addr} connected.")
 
     connected = True
     while connected:
-        msg = conn.recv(HEADER).decode('utf-8')
-        # blank message is sent the first time we connect, make sure we get actual message
-        if msg:
-            # strip padded part of buffer away
-            header = msg.strip().split('|', 1)
-            msg_length = int(header[0])
-            msg_type = header[1]
+        try:
+            msg = conn.recv(HEADER).decode('utf-8')
+            # blank message is sent the first time we connect, make sure we get actual message
+            if msg:
+                # strip padded part of buffer away
+                header = msg.strip().split('|', 1)
+                msg_length = int(header[0])
+                msg_type = header[1]
 
-            if msg_type == 'TEL':
-                print("sending telemetry data")
-                conn.send("Telemetry data:".encode('utf-8'))    
-            elif msg_type == 'MAP':
-                print("sending map data")
-                conn.send("Map data:".encode('utf-8'))    
+                if msg_type == 'TEL':
+                    print("sending telemetry data")
+                    conn.send("Telemetry data:".encode('utf-8'))    
+                elif msg_type == 'MAP':
+                    print("sending map data")
+                    conn.send("Map data:".encode('utf-8'))    
 
-            if msg == DISCONNECT_MESSAGE:
-                connected = False
+                if msg == DISCONNECT_MESSAGE:
+                    connected = False
+        except OSError:
+            break
 
-    conn.close()
         
 
 def start():
+    clients = list()
     server.listen()
     print(f"[LISTENING] server is listening on {SERVER}")
     while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target = handle_client, args=(conn, addr))
-        thread.start()
-        print("[ACTIVE CONNECTIONS] {}".format(threading.activeCount() - 1))
+        try:
+            conn, addr = server.accept()
+            clients.append(conn)
+            thread = threading.Thread(target = handle_client, args=(conn, addr))
+            thread.start()
+            print("[ACTIVE CONNECTIONS] {}".format(threading.activeCount() - 1))
+        except OSError:
+            for conn in clients:
+                conn.close()
+            break
 
 def main():
-    print("[STARTING] server is starting...")
-    start()
-
+    thread = threading.Thread(target = start)
+    thread.start()
+    while True:
+        try:
+            sleep(1)
+        except KeyboardInterrupt:
+            server.close()
+            break
 if __name__ == "__main__":
     main()
